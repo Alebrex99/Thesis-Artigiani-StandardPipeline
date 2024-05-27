c34ad45d91b5e010109f93bf79a5467d3a27a244
Alebrex99 <ale.brex99@gmail.com>
2024-05-27 20:43:56 +0200

    Fix chai visible + loading

--- Assets/_Scripts/Interactions/LoadingTrigger.cs
+++ Assets/_Scripts/Interactions/LoadingTrigger.cs
@@ -8,15 +8,10 @@ public class LoadingTrigger : MonoBehaviour
     //OGGETTI DA IGNORARE:
     [SerializeField] private GameObject[] goToIgnore;
     [SerializeField] private cMenuLoad cMenuLoad;
-    private bool isMenu = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(cMenuLoad != null && cMenuLoad.IsShowing())
-        {
-            isMenu = true;
-        }
         
     }
 
@@ -28,23 +23,23 @@ public class LoadingTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isMenu)
+        Debug.Log("ENTRATO: " +  other.gameObject.name); //es. entra il button load + entra il collider (game object nel bottone che si chiama collider)
+        if (cMenuLoad.IsShowing())
         {
-
-            if (!goToIgnore.Contains<GameObject>(other.gameObject))
+            if (!goToIgnore.Contains(other.gameObject))
             {
                 other.gameObject.SetActive(false);
-                Debug.Log("DEVE ESSERCI SOLO IL MENU");
+                Debug.Log("DEVE ESSERCI SOLO IL MENU : " + other.gameObject.name);
             }
             //se in questo trigger si trovano i goToIgnore, non devono essere disattivati
         }
         else //alora è solo loading cambio scena
         {
-            //se il pannello attivo è quello di loading allora disattiva tutti gli oggetti col trigger eccetto la OVR camera rig
-            if (!other.gameObject.Equals(goToIgnore[0]))
+            //se il pannello attio è quello di loading allora disattiva tutti gli oggetti col trigger eccetto la OVR camera rig
+            if (other.gameObject != GetComponent<OVRCameraRig>())
             {
                 other.gameObject.SetActive(false);
-                Debug.Log("DEVE ESSERCI SOLO IL LOADING");
+                Debug.Log("DEVE ESSERCI SOLO IL LOADING" + other.gameObject.name);
             }
         }
          
